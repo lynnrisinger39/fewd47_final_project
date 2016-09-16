650886fc27ac4cce81288881e62f2551032db310
Lynn Risinger <lynn.risinger1@gmail.com>
2016-09-15 21:13:07 -0700

    Version 1 of FEWD 47 Final Project

--- js/athletes.js
+++ js/athletes.js
@@ -1,5 +1,4 @@
 // toggles paragraph when user clicks on athletes image 
 $('.athletes').find('img').on('click', function() {
-	$(this).next().fadeToggle(300);
-
+	$(this).next().slideToggle(300);
 });
